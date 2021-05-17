20142681822657ece9cca7554d7c88664dffc95b
thauandev <thauandepaula@icloud.com>
2021-05-17 08:29:51 -0300

    Realizando refresh do token

--- services/api.ts
+++ services/api.ts
@@ -1,44 +1,82 @@
-import axios,{AxiosError} from "axios";
-import { parseCookies, setCookie } from 'nookies'
+import axios, { AxiosError } from "axios";
+import { parseCookies, setCookie } from "nookies";
 
-let cookies = parseCookies()
+let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestQueue = [];
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: "http://localhost:3333",
   headers: {
-    Authorization: `Bearer ${cookies['jwt.token']}`
+    Authorization: `Bearer ${cookies["jwt.token"]}`,
+  },
+});
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response.status === 401) {
+      if (error.response.data?.code === "token.expired") {
+        cookies = parseCookies();
+
+        const { "jwt.token": refreshToken } = cookies;
+        const originalConfig = error.config;
+
+        if (!isRefreshing) {
+          isRefreshing = true;
+
+          api
+            .post("/refresh", {
+              refreshToken,
+            })
+            .then((response) => {
+              const { token } = response.data;
+
+              setCookie(undefined, "jwt.token", token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: "/",
+              });
+              setCookie(
+                undefined,
+                "jwt.refreshToken",
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: "/",
+                }
+              );
+
+              api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+              failedRequestQueue.forEach((request) => request.onSuccess(token));
+              failedRequestQueue = [];
+            })
+            .catch((error) => {
+              failedRequestQueue.forEach((request) => request.onFailure(error));
+              failedRequestQueue = [];
+            })
+            .finally(() => {
+              isRefreshing = false;
+            });
+        }
+
+        return new Promise((resolve, reject) => {
+          failedRequestQueue.push({
+            onSuccess: (token: string) => {
+              originalConfig.headers["Authorization"] = `Bearer ${token}`;
+
+              resolve(api(originalConfig));
+            },
+
+            onFailure: (err: AxiosError) => {
+              reject(err);
+            },
+          });
+        });
+      } else {
+      }
+    }
   }
-})
-
-api.interceptors.response.use(response => {
-  return response;
-}, (error: AxiosError) => {
-   if(error.response.status === 401) {
-     if(error.response.data?.code ===  'token.expired') {
-       cookies = parseCookies()
-
-       const { 'jwt.token': refreshToken } = cookies;
-
-       api.post('/refresh', {
-         refreshToken,
-       }).then(response => {
-         const { token } = response.data
-
-
-      setCookie(undefined, "jwt.token", token, {
-        maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: "/",
-      });
-      setCookie(undefined, "jwt.refreshToken", response.data.refreshToken, {
-        maxAge: 60 * 60 * 24 * 30, // 30 days
-        path: "/",
-      });
-
-      api.defaults.headers["Authorization"] = `Bearer ${token}`; 
-
-       })
-     } else {
-
-     }
-   }
-})
+);
